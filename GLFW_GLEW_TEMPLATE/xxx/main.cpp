bc5aa50f6526a25268b2d3c98cf237e9b6e9c4fa
Vincent Smithers <vsmithers1087@gmail.com>
2016-09-19 08:20:12 -0700

    finish section

--- GLFW_GLEW_TEMPLATE/xxx/main.cpp
+++ GLFW_GLEW_TEMPLATE/xxx/main.cpp
@@ -11,34 +11,51 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-
+//MARK: key callback declaration
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
+const GLuint WIDTH = 800, HEIGHT = 600;
 
+//MARK: vertex shader c string
+const GLchar* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}\0";
+
+//MARK: fragment shader c string
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(0.2f, 0.1f, 0.8f, 1.0f);\n"
+"}\n\0";
+
+//MARK: main
 int main() {
-    std::cout << "Starting GLFW context" << std::endl;
-    
-    //glew init
-    
+    //MARK: glew init
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
     
     if (!glfwInit()) {
         std::cout<< "Couldn't init glfw" << std::endl;
         exit(1);
     }
 
-    //create window
+    std::cout << "Starting GLFW context" << std::endl;
     
-    GLFWwindow *window;
+    //MARK: set required options for glfw window
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     glewExperimental = GL_TRUE;
     
-    window = glfwCreateWindow(900, 640.0, "Test_Window", NULL, NULL);
+    //MARK: create window
+ 
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Test_Window", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
     
     if (!window) {
         std::cout<< "Couldn't create window" << std::endl;
@@ -46,31 +63,117 @@ int main() {
         exit(EXIT_FAILURE);
     }
     
+    //MARK: set callback
     glfwSetKeyCallback(window, key_callback);
     
-    //set viewport.
+    //MARK: glew init
+    glewInit();
     
-    glfwMakeContextCurrent(window);
-    
+    //MARK: set viewport.
     int width, height;
-    
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
     
+    //MARK: vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    
+    //MARK: fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    
+    //MARK: shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+ 
+    //MARK: check shader success
+    
+    GLint success;
+    GLchar infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    //MARK: vertices
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    //MARK: indices
+    GLuint indices[] = {  // Note that we start from 0!
+        0, 1, 3,  // First Triangle
+        1, 2, 3   // Second Triangle
+    };
+    
+    //MARK: linking vertex attributes
+    GLuint VAO, VBO, EBO;
+    
+    //MARK: bind vertex array object then bind and set vertex buffers and attribute pointers
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    
+    glBindVertexArray(VAO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    
+    //MARK: undbind array buffers
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    
+//    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    
+    //MARK: "RUNTIME LOOP"
     while (!glfwWindowShouldClose(window)) {
+        //MARK: check for events
         glfwPollEvents();
         
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        //MARK: render
+        glClearColor(0.2f, 0.9f, 0.3f, 1.0f);
+        
+        //MARK: clear color buffer
         glClear(GL_COLOR_BUFFER_BIT);
         
+        //MARK: draw triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+        
+        //MARK: swap screen buffers
         glfwSwapBuffers(window);
     }
+    //MARK: deallocate once loop exits
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     
+    //MARK: terminate glfw
     glfwTerminate();
     
     return 0;
 }
 
+//MARK: keycallback defined
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode){
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
         std::cout<< "ESC Pressed" << std::endl;
