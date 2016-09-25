cff656989eb251526c678c82719cc83b7ca2a6f1
Vincent Smithers <vsmithers1087@gmail.com>
2016-09-25 15:52:38 -0700

    finish

--- GLFW_GLEW_TEMPLATE/xxx/main.cpp
+++ GLFW_GLEW_TEMPLATE/xxx/main.cpp
@@ -129,7 +129,7 @@ int main() {
     
     // MARK: load and create image texture
     int widthX, heightX;
-    unsigned char* image = SOIL_load_image("testX.jpg", &widthX, &heightX, 0, SOIL_LOAD_RGB);
+    unsigned char* image = SOIL_load_image("turtles.jpg", &widthX, &heightX, 0, SOIL_LOAD_RGB);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthX, heightX, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
     glGenerateMipmap(GL_TEXTURE_2D);
     SOIL_free_image_data(image);
@@ -147,7 +147,7 @@ int main() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    image = SOIL_load_image("container.jpg", &widthX, &heightX, 0, SOIL_LOAD_RGB);
+    image = SOIL_load_image("turtlesTex.jpg", &widthX, &heightX, 0, SOIL_LOAD_RGB);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthX, heightX, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
     glGenerateMipmap(GL_TEXTURE_2D);
     SOIL_free_image_data(image);
